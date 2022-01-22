5c59a1915e86f995a954d438b9e37971bec65ed2
KurtSpiker <kurt.spiker@gmail.com>
2022-01-21 23:39:04 -0700

    Create daydreamingStockbroker.js

---
+++ daydreamingStockbroker.js
@@ -0,0 +1,5 @@
+// Input
+//The first line of input contains an integer d (1≤d≤365), the number of days that Gina goes back in time in her daydream. Then follow d lines, the i’th of which contains an integer pi (1≤pi≤500) giving the price at which Gina can buy or sell stock in Rollercoaster Inc. on day i. Days are ordered from oldest to newest.
+
+// Output
+//Output the maximum possible amount of money Gina can have on the last day. Note that the answer may exceed 232.

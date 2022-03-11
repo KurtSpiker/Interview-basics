4044090eba2d8fa442fc92c6bfe50a876585686b
KurtSpiker <kurt.spiker@gmail.com>
2022-03-10 22:58:32 -0700

    Update romanToInt.js

--- romanToInt.js
+++ romanToInt.js
@@ -16,3 +16,18 @@
 // X can be placed before L (50) and C (100) to make 40 and 90. 
 // C can be placed before D (500) and M (1000) to make 400 and 900.
 // Given a roman numeral, convert it to an integer.
+
+
+romanToInt = (romanString) => {
+  let conversions = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  }
+  
+  
+}
